29e229dc8bb1d4811bfc94b46513e342bd418bd2
sonninnos <tony@pihaus.org>
2024-05-04 18:45:49 +0300

    cv1k/ep1c12 subtarget hack

--- scripts/src/video.lua
+++ scripts/src/video.lua
@@ -1696,3 +1696,22 @@ if (VIDEOS["ZR36110"]~=null) then
 		MAME_DIR .. "src/devices/video/zr36110.h",
 	}
 end
+
+-- cv1k/ep1c12 subtarget hack
+if _OPTIONS["subtarget"]=="arcade" then
+	files {
+		MAME_DIR .. "src/mame/cave/ep1c12.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12.h",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit0.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit1.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit2.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit3.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit4.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit5.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit6.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit7.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit8.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12in.ipp",
+		MAME_DIR .. "src/mame/cave/ep1c12pixel.ipp",
+	}
+end
