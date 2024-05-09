a8d87bb0d923897f8a2ef311a7cffeeef5196180
Eric Warmenhoven <eric@warmenhoven.org>
2024-05-09 01:22:57 -0400

    iOS: plist should not be embedded in dylib

--- scripts/src/main.lua
+++ scripts/src/main.lua
@@ -418,7 +418,7 @@ if (STANDALONE==true) then
 end
 
 if (STANDALONE~=true) then
-	if _OPTIONS["targetos"]=="macosx" and (not override_resources) then
+	if _OPTIONS["targetos"]=="macosx" and (not override_resources) and not (_OPTIONS["LIBRETRO_IOS"]=="1" or _OPTIONS["LIBRETRO_TVOS"]=="1") then
 		local plistname = _target .. "_" .. _subtarget .. "-Info.plist"
 		linkoptions {
 			"-sectcreate __TEXT __info_plist " .. _MAKE.esc(GEN_DIR) .. "resource/" .. plistname
